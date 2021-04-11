89849a6e76d1b69b5569a33ffa812631af2f8935
supersaiyane <gurpreet.singh_89@outlook.com>
2021-04-11 10:46:18 +0530

    first commit

---
+++ src/test/java/com/supersaiyane/springrestservices/SpringbootBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supersaiyane.springrestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
